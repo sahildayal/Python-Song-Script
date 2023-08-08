fdd13ca80550c24ecdc634a056ae8d558519b970
Sahil Dayal <sd8804@rit.edu>
2023-08-08 09:07:40 -0400

    Added constants and necessary functions

---
+++ main.py
@@ -0,0 +1,13 @@
+SPOTIFY_CLIENT_ID = ""
+SPOTIFY_CLIENT_SECRET = ""
+YOUTUBE_API_KEY = ""
+
+def get_spotify_playlist_tracks(playlist_id):
+    pass
+
+def seach_yt_musicvideos(query):
+    pass
+
+def download_convert_mp3(vidoo_id):
+    pass
+
